b49d6ce7e48d8dcc4ed22f9e242e357855e17c39
Dominic Rooney <99553929+orangutanrider@users.noreply.github.com>
2024-12-30 22:25:16 +0000

    reception
    reception is currently "blocking" because the system will wait for the all the file reads to finish before making any requests.
    
    A non-blocking implementation would read list in chunks and make requests as it does so; You would only care to do this for very large files, so it will go un-implemented for the foreseeable future.

--- process/src/reception.rs
+++ process/src/reception.rs
@@ -1,193 +1,86 @@
-//! File intake as of cli input
+mod blocking;
+mod blocking_thread;
+use blocking::*;
+use blocking_thread::*;
 
-// Logging todo
-// probably replace the prints with logging
+// large file input could mean reading that file in chunks and streaming it to the system, instead of halting until the entire file gets read.
+// i.e. a non-blocking reception
 
 use locatch_macro::*;
+use locatch_macro::unsafe_lib::UnsafeSend;
+
 use crate::cli::*;
 use crate::serial_input::{Config, FilenameMacro, List, TicketMacro};
 
-use std::{fs, path::PathBuf};
+use tokio::join;
 
-/// The output of reception
-/// Deserialized files, from the CLI input.
 pub type RecievedInput = (Config, List);
 
 #[inline]
-pub fn reception(cli: &Cli) -> Result<RecievedInput, ()> {
-    // Recieve inputs
-    let config = match config_reception(&cli.config) {
+pub async fn reception(cli: &Cli) -> Result<RecievedInput, LocatchErr> {
+    return blocking_reception(cli).await
+}
+
+#[inline]
+async fn blocking_reception(cli: &Cli) -> Result<RecievedInput, LocatchErr> {
+    // potentially large file reception
+    let list = tokio::spawn(unsafe { required_reception_thread::<List>(UnsafeSend(&cli.list)) });
+
+    // small file reception
+    let config = fallback_reception::<Config>(&cli.config, CONFIG_FALLBACK);
+    let filename_macro = optional_reception::<FilenameMacro>(&cli.filename_macro);
+    let ticket_macro = optional_reception::<TicketMacro>(&cli.ticket_macro);
+
+    // await
+    let (config, filename_macro, ticket_macro) = join!(config, filename_macro, ticket_macro);
+
+    // unwraps
+    let config = match config {
         Ok(ok) => ok,
-        Err(err) => return Err(err),
+        Err(err) => {
+            list.abort();
+            return Err(err)
+        },
+    };
+    
+    let filename_macro = match filename_macro {
+        Ok(ok) => ok,
+        Err(err) => {
+            list.abort();
+            return Err(err)
+        },
     };
 
-    let mut list = match list_reception(&cli.list) {
+    let ticket_macro = match ticket_macro {
         Ok(ok) => ok,
-        Err(err) => return Err(err),
+        Err(err) => {
+            list.abort();
+            return Err(err)
+        },
     };
 
-    let filename_macro = match filename_macro_reception(&cli.filename_macro) {
-        Ok(ok) => ok,
-        Err(err) => return Err(err),
+    // await
+    let mut list = match list.await {
+        Ok(ok) => match ok {
+            Ok(ok) => ok,
+            Err(err) => return Err(err),
+        },
+        Err(_) => panic!(),
     };
 
-    let ticket_macro = match ticket_macro_reception(&cli.ticket_macro) {
-        Ok(ok) => ok,
-        Err(err) => return Err(err),
-    };
-
-    // Apply macros
-    list.apply_internal_macros();
-    match filename_macro {
-        Some(filename_macro) => list.apply_filename_macro(&filename_macro),
-        None => {/* Do Nothing */},
-    }
+    // apply macros
     match ticket_macro {
-        Some(ticket_macro) => list.apply_ticket_macro(&ticket_macro),
-        None => {/* Do Nothing */},
+        Some(val) => list.apply_ticket_macro(&val),
+        None => {/* Do nothing */},
     }
 
-    // Return
+    match filename_macro {
+        Some(val) => list.apply_filename_macro(&val),
+        None => {/* Do nothing */},
+    }
+
+    list.apply_internal_macros();
+
+    // return
     return Ok((config, list))
-}
-
-/// Gets config, and deserializes it.
-/// Config has a fallback path associated with it, if it was not inputted via cli, then the system will try to get it via the default path.
-#[inline]
-fn config_reception(cli: &Option<PathBuf>) -> Result<Config, ()> {
-    // Recieve config
-    let config = match cli {
-        // Path was inputted via cli
-        Some(config) => {
-            match fs::read_to_string(config) {
-                Ok(ok) => {
-                    println!("Config file recieved");
-                    ok
-                },
-                Err(err) => {
-                    println!("Error with config file: {}", err);
-                    return Err(())
-                },
-            }
-        },
-        // No path was inputted
-        None => {
-            match fs::read_to_string(CONFIG_PATH) {
-                Ok(ok) => {
-                    println!("Config file recieved");
-                    ok
-                },
-                Err(err) => {
-                    println!("Error with config file: {}", err);
-                    return Err(())
-                },
-            }
-        },
-    };
-
-    // Deserialize config
-    let config = match Config::from_json(&config) {
-        Ok(ok) => ok,
-        Err(err) => {
-            println!("Error with deserialization of config file: {}", err);
-            return Err(())
-        },
-    };
-
-    return Ok(config)
-}
-
-/// Gets list, and deserializes it
-#[inline]
-fn list_reception(cli: &PathBuf) -> Result<List, ()> {
-    // Recieve list
-    let list = match fs::read_to_string(cli) {
-        Ok(ok) => {
-            println!("List file recieved");
-            ok
-        },
-        Err(err) => {
-            println!("Error with list file: {}", err);
-            return Err(())
-        },
-    };
-
-    // Deserialize list
-    let list = match List::from_json(&list) {
-        Ok(ok) => ok,
-        Err(err) => {
-            println!("Error with deserialization of list file: {}", err);
-            return Err(())
-        },
-    };
-
-    return Ok(list)
-}
-
-#[inline]
-fn filename_macro_reception(cli: &Option<PathBuf>) -> Result<Option<FilenameMacro>, ()> {
-    let cli = match cli {
-        Some(val) => val,
-        None => {
-            println!("No filename macro input, outside of list");
-            return Ok(None)
-        },
-    };
-
-    // Recieve file
-    let serial = match fs::read_to_string(cli) {
-        Ok(ok) => {
-            println!("Filename macro file recieved");
-            ok
-        },
-        Err(err) => {
-            println!("Error with filename macro: {}", err);
-            return Err(())
-        },
-    };
-
-    // Deserialize file
-    let deserial = match FilenameMacro::from_json(&serial) {
-        Ok(ok) => ok,
-        Err(err) => {
-            println!("Error with deserialization of filename macro: {}", err);
-            return Err(())
-        },
-    };
-
-    return Ok(Some(deserial))
-}
-
-#[inline]
-fn ticket_macro_reception(cli: &Option<PathBuf>) -> Result<Option<TicketMacro>, ()> {
-    let cli = match cli {
-        Some(val) => val,
-        None => {
-            println!("No ticket macro input, outside of list");
-            return Ok(None)
-        },
-    };
-
-    // Recieve file
-    let serial = match fs::read_to_string(cli) {
-        Ok(ok) => {
-            println!("Ticket macro file recieved");
-            ok
-        },
-        Err(err) => {
-            println!("Error with ticket macro file: {}", err);
-            return Err(())
-        },
-    };
-
-    // Deserialize file
-    let deserial = match TicketMacro::from_json(&serial) {
-        Ok(ok) => ok,
-        Err(err) => {
-            println!("Error with deserialization of ticket macro: {}", err);
-            return Err(())
-        },
-    };
-
-    return Ok(Some(deserial))
 }
